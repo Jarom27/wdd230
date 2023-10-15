300dc8765d88fa7c9425da1d07c2f33ddafd5ae9
Jarom Mariscal <jaromero_marz234@outlook.com>
2023-10-14 21:05:56 -0700

    Dark Mode added

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -8,3 +8,18 @@ const update = document.getElementById("updated");
 update.innerHTML = `${document.lastModified}`;
 const year = document.getElementById("year");
 year.innerHTML = `©${new Date().getFullYear()} `;
+
+/*Dark mode*/
+const darkSwitch = document.querySelector(".switch");
+const checkbox = document.querySelector(".switch input");
+darkSwitch.addEventListener("click", () => {
+    const header = document.querySelector("header");
+    if (checkbox.checked == true) {
+        header.classList.toggle("dark");
+        document.body.classList.toggle("dark");
+    }
+    else {
+        header.classList.remove("dark");
+        document.body.classList.remove("dark");
+    }
+});
